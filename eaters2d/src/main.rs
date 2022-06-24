09e7f2132e696e0bbb49468ded39c9abcc846a7b
Tom Selfin <selfint@gmail.com>
2022-06-24 18:55:12 +0300

    initial commit

---
+++ eaters2d/src/main.rs
@@ -0,0 +1,41 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(greet_people)
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
